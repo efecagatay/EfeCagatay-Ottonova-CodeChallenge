c9836130ed86cb0496c903d868b36a6cf6bd0e1c
efecagatay <93163361+efecagatay@users.noreply.github.com>
2026-03-09 14:34:00 +0100

    a

--- tests/insurance-calculator.spec.ts
+++ tests/insurance-calculator.spec.ts
@@ -34,7 +34,7 @@ test.describe ('Digital Assistant - Insurance Contribution Calculator' , () => {
         const calculatedBirthYear = (currentYear - data.age).toString();
         await insuranceCalculator.einkommenTextbox.fill('80000');
         await insuranceCalculator.employmentWeiterButton.click();
-        await insuranceCalculator.selectInsuranceStartDate('01.06.2026');
+        await insuranceCalculator.selectInsuranceStartDateNextMonth()
         await insuranceCalculator.insuranceWeiterButton.click();
         await insuranceCalculator.selectBirthDay(data.birthdayTag, data.birthdayMonth , calculatedBirthYear);
 
